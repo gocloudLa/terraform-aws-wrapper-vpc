d324caccef028c4cd9c0f3f6d9fbaa0e8d218f95
Guido Montano <gmontano@gocloud.la>
2026-02-27 11:00:24 -0300

    feat: update examples for new implementation

---
+++ examples/01-complete-vpc/data_sources.tf
@@ -0,0 +1,30 @@
+data "aws_iam_policy_document" "dynamodb_endpoint_policy" {
+  statement {
+    effect    = "Allow"
+    actions   = ["dynamodb:*"]
+    resources = ["*"]
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "s3_endpoint_policy" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:*"]
+    resources = ["*"]
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+  }
+}
+
+data "aws_region" "current" {}
+data "aws_availability_zones" "available" {
+  state = "available"
+}
