c819d0ce5e7d1cbaf9e99e18134f4ad5e69b1a6e
Federico Midaglia <fmidaglia@gocloud.la>
2026-04-19 12:10:22 -0300

    fix(ec2-natgw): output.tf

--- modules/aws/terraform-aws-nat-gateway/output.tf
+++ modules/aws/terraform-aws-nat-gateway/output.tf
@@ -3,5 +3,5 @@ output "aws_nat_gateway_id" {
 }
 
 output "ec2_nat_gateway_id" {
-  value = var.kind == "ec2" ? module.vpc-ec2-nat-gateway[0].primary_network_interface_id : null
+  value = var.kind == "ec2" ? module.ec2_nat_gateway[0].primary_network_interface_id : null
 }
