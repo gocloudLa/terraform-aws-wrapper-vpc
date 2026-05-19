ac238eb6ebe6640e5fc21a5d381885c86ca4f8fe
Guido Montano <gmontano@gocloud.la>
2026-05-19 16:14:29 -0300

    feature: add support for custom individual tags for each resource

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ module "vpc" {
   dhcp_options_netbios_name_servers = lookup(each.value, "dhcp_options_netbios_name_servers", [])
   dhcp_options_netbios_node_type    = lookup(each.value, "dhcp_options_netbios_node_type", "")
 
-  tags = merge(lookup(each.value, "tags", local.common_tags), { Name = "${local.custom_common_name[each.key]}" })
+  tags = merge(local.common_tags, { Name = "${local.custom_common_name[each.key]}" }, try(each.value.tags, {}))
 
 }
 
@@ -47,14 +47,15 @@ locals {
             enable_ipv6             = lookup(vpc_config, "enable_ipv6", false)
             create_egress_only_igw  = lookup(internet_gateway_values, "create_egress_only_igw", false)
             tags = merge(
-              lookup(internet_gateway_values, "tags", local.common_tags),
+              local.common_tags,
               {
                 Name = (
                   internet_gateway_name == "" ?
                   local.custom_common_name[vpc_key] :
                   "${local.custom_common_name[vpc_key]}-${internet_gateway_name}"
                 )
-              }
+              },
+              try(internet_gateway_values.tags, {})
             )
         })
       } if((length(lookup(vpc_config, "internet_gateway", {})) > 0))
@@ -87,7 +88,7 @@ locals {
           create_network_acl = lookup(network_acl_values, "create_network_acl", true)
           vpc_key            = vpc_key
           rules              = lookup(network_acl_values, "rules", {})
-          tags               = merge(lookup(network_acl_values, "tags", local.common_tags), { Name = "${local.custom_common_name[vpc_key]}-${network_acl_name}" })
+          tags               = merge(local.common_tags, { Name = "${local.custom_common_name[vpc_key]}-${network_acl_name}" }, try(network_acl_values.tags, {}))
         }
       } if((length(lookup(vpc_config, "network_acl", {})) > 0))
     ]
@@ -179,7 +180,7 @@ resource "aws_default_network_acl" "this" {
     ignore_changes = [subnet_ids]
   }
 
-  tags = merge(lookup(each.value, "tags", local.common_tags), { Name = "${local.custom_common_name[each.key]}-default" })
+  tags = merge(local.common_tags, { Name = "${local.custom_common_name[each.key]}-default" }, try(each.value.tags, {}))
 }
 
 ## Route Table
@@ -193,7 +194,7 @@ locals {
           vpc_key              = vpc_key
           create_route_table   = lookup(route_table_values, "create_route_table", true)
           create_default_route = lookup(route_table_values, "default_route", {}) == {} ? false : true
-          tags                 = merge(local.common_tags, { Name = "${local.custom_common_name[vpc_key]}-${route_table_name}" })
+          tags                 = merge(local.common_tags, { Name = "${local.custom_common_name[vpc_key]}-${route_table_name}" }, try(route_table_values.tags, {}))
         }
       } if((length(lookup(vpc_config, "route_table", {})) > 0))
     ]
@@ -223,7 +224,7 @@ resource "aws_default_route_table" "this" {
 
   route = []
 
-  tags = merge({ Name = "${local.custom_common_name[each.key]}-default" }, lookup(each.value, "tags", local.common_tags))
+  tags = merge(local.common_tags, { Name = "${local.custom_common_name[each.key]}-default" }, try(each.value.tags, {}))
 }
 
 ## Subnets
@@ -262,7 +263,7 @@ locals {
             attach_nacl = lookup(subnet_values, "network_acl", "") != "" ? true : false
             network_acl = try(module.network_acl["${vpc_key}-${subnet_values.network_acl}"].id, subnet_values.network_acl_id, "")
 
-            tags = lookup(subnet_values, "tags", merge(local.common_tags, { Name = "${local.custom_common_name[vpc_key]}-${subnet_group_name}-${subnet_name}" }))
+            tags = merge(local.common_tags, { Name = "${local.custom_common_name[vpc_key]}-${subnet_group_name}-${subnet_name}" }, try(subnet_values.tags, {}))
 
           }
         } if((length(lookup(vpc_config, "subnets", {})) > 0))
@@ -313,7 +314,7 @@ locals {
           kind               = lookup(nat_gateway_values, "kind", "aws")
           subnet             = lookup(nat_gateway_values, "subnet", null)
           nat_parameters     = lookup(nat_gateway_values, "nat_parameters", { connectivity_type = "public" })
-          tags               = merge(lookup(nat_gateway_values, "tags", local.common_tags), { Name = "${local.custom_common_name[vpc_key]}-${nat_gateway_name}" })
+          tags               = merge(local.common_tags, { Name = "${local.custom_common_name[vpc_key]}-${nat_gateway_name}" }, try(nat_gateway_values.tags, {}))
         }
       } if((length(lookup(vpc_config, "nat_gateway", {})) > 0))
     ]
@@ -374,7 +375,7 @@ locals {
           local_gateway_id = try(route_table_values.default_route.local_gateway_id, null)
           #try(module.nat_gateway["${vpc_key}-${route_table_values.default_route.local_gateway}"].ec2_nat_gateway_id, route_table_values.default_route.local_gateway_id, null)
 
-          tags = merge(local.common_tags, { Name = "${local.custom_common_name[vpc_key]}-${route_table_name}" })
+          tags = merge(local.common_tags, { Name = "${local.custom_common_name[vpc_key]}-${route_table_name}" }, try(route_table_values.tags, {}))
         }
       } if((length(lookup(route_table_values, "default_route", {})) > 0))
     ]
@@ -421,7 +422,7 @@ locals {
             local_gateway_id = try(route_table_values.route_values.local_gateway_id, null)
             #try(module.nat_gateway["${vpc_key}-${route_table_values.route_values.local_gateway}"].ec2_nat_gateway_id, route_table_values.route_values.local_gateway_id, null)
 
-            tags = merge(local.common_tags, { Name = "${local.custom_common_name[vpc_key]}-${route_table_name}" })
+            tags = merge(local.common_tags, { Name = "${local.custom_common_name[vpc_key]}-${route_table_name}" }, try(route_values.tags, {}))
           }
         }
       }
@@ -469,7 +470,7 @@ locals {
           flow_log_hive_compatible_partitions             = lookup(flow_logs_values, "flow_log_hive_compatible_partitions", false)
           flow_log_per_hour_partition                     = lookup(flow_logs_values, "flow_log_per_hour_partition", false)
           vpc_id                                          = module.vpc[vpc_key].vpc_id
-          tags                                            = merge(local.common_tags, { Name = "${local.custom_common_name[vpc_key]}-${flow_logs_name}" })
+          tags                                            = merge(local.common_tags, { Name = "${local.custom_common_name[vpc_key]}-${flow_logs_name}" }, try(flow_logs_values.tags, {}))
         }
       } if((length(lookup(vpc_config, "flow_logs", {})) > 0))
     ]
@@ -520,7 +521,7 @@ locals {
           private_dns_enabled = try(endpoint_values.private_dns_enabled, false)
           security_group_ids  = try(endpoint_values.security_group_ids, [])
 
-          tags = merge(local.common_tags, { Name = "${local.custom_common_name[vpc_key]}-${endpoint_values.service}-vpc-endpoint" })
+          tags = merge(local.common_tags, { Name = "${local.custom_common_name[vpc_key]}-${endpoint_values.service}-vpc-endpoint" }, try(endpoint_values.tags, {}))
         }
       }
     }
@@ -537,18 +538,3 @@ module "vpc_endpoint" {
   endpoints = local.create_endpoints[each.key]
 
 }
-
-# module "vpc_peering" {  
-#   source = "./modules/aws/terraform-aws-vpc-peering"
-
-#   for_each = var.vpc__parameters
-
-#   peer_owner_id =
-#   peer_vpc_id =
-#   vpc_id = 
-#   auto_accept = 
-#   peer_region = 
-#   accepter = 
-#   requester =
-#   tags = 
-# }
